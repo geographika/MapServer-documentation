759d054b796dbf57ca8b5707920deacdc13e9411
sethg <sethg@geographika.co.uk>
2024-09-22 15:58:47 +0200

    Test

--- scripts/ci_deploy_website.sh
+++ scripts/ci_deploy_website.sh
@@ -4,6 +4,7 @@ builddir=$1
 destdir=$2
 sha=$3
 
+# check the PDF was created and upload to https://mapserver.org/pdf/MapServer.pdf
 if [ -f $builddir/latex/en/MapServer.pdf ]; then
   set -x
   scp $builddir/latex/en/MapServer.pdf mapserver@mapserver.org:/var/www/mapserver.org/pdf/
@@ -12,9 +13,22 @@ fi
 
 
 if [ ! -d $destdir/mapserver.github.io ]; then
-  git clone git@github.com:mapserver/mapserver.github.io.git $destdir/mapserver.github.io
+  git clone git@github.com:geographika/mapserver.github.io.git $destdir/mapserver.github.io
 fi
 
+# specify the files and directories to keep
+keep_files=(.git README.md .nojekyll)
+
+# delete all items in the repository
+for item in * .[^.]*; do
+    # Check if the item is not in the keep_files array
+    if [[ ! " ${keep_files[@]} " =~ " ${item} " ]]; then
+        # Remove the item (directory or file)
+        rm -rf "$item"
+    fi
+done
+
+# copy in the newly created files
 cd $builddir/html
 cp -rf * $destdir/mapserver.github.io
 
